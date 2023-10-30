a2795b8a812468ecb72ad4cd3889eca4c10a4a1d
Josh B <bowenpjoshua@gmail.com>
2023-10-30 11:43:45 -0400

    Added codes and began troubleshooting. nothing works currently but it seems to be a decent-ish basis.

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const db = require('./config/database');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Handlebars setup
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    extname: '.handlebars'
+}));
+app.set('view engine', 'handlebars');
+
+// Start the database connection
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+// Routes
+const taskController = require('./controllers/taskControllers');
+
+// Ensure that taskController.getAllTasks is a function
+if (typeof taskController.getAllTasks === 'function') {
+    app.get('/', taskController.getAllTasks);
+} else {
+    console.error('Error: taskController.getAllTasks is not a function');
+}
+
+// Server start
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ controllers/taskControllers.js
@@ -0,0 +1,20 @@
+const Task = require('../models/taskModel');
+
+const TaskController = {
+  getAllTasks: async (req, res) => {
+    try {
+      const tasks = {
+        todo: await Task.getAllByStatus('todo'),
+        inwork: await Task.getAllByStatus('inwork'),
+        completed: await Task.getAllByStatus('completed')
+      };
+      res.render('index', { tasks });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  // Other controller actions for CRUD and moving tasks
+};
+
+module.exports = TaskController;
