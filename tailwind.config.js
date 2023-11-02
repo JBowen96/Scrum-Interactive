8d4c9849c144e5bf6bda2184bfc3b9766b8d8a8f
Zach <itsaboy3366@gmail.com>
2023-11-01 20:53:39 -0400

    changed darkmode config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./views/**/*.{html, js, handlebars}", "node_modules/preline/dist/*.js"],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
