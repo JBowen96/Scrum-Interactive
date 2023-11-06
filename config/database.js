254f4a146bb9a42154ec8fde59dbb39b6702070b
Zach <itsaboy3366@gmail.com>
2023-11-06 14:33:52 -0500

    styling/layout for sign-up and login page

--- config/database.js
+++ config/database.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'J',
+  host: 'database-1.cyi0wfbnivj1.us-east-1.rds.amazonaws.com',
+  user: 'admin',
   port: 3306,
   password: '1crappypassword',
-  database: 'task_app1'
 });
   
 module.exports = db;
