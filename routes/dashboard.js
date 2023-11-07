328c33ca4d86962226a73090898b7a1ca887db00
Josh B <bowenpjoshua@gmail.com>
2023-11-07 11:31:08 -0500

    added function for logout and dashboard button reloads page

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -66,4 +66,17 @@ router.delete('/delete-card/:id', async (req, res) => {
     }
 });
 
+router.post('/logout', (req, res) => {
+    // Destroy the session to log the user out
+    req.session.destroy((err) => {
+        if (err) {
+            console.error('Error destroying session:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            // Redirect the user to the login page after logout
+            res.redirect('/login');
+        }
+    });
+});
+
 module.exports = router;      
