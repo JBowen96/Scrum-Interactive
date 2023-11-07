d679471f4a4fff2f637a4870970d62f3a891c37f
Zach <itsaboy3366@gmail.com>
2023-11-06 22:11:48 -0500

    styling changes

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
-  host: 'localhost',
-  username: 'J',
+  host: 'database-1.cyi0wfbnivj1.us-east-1.rds.amazonaws.com',
+  username: 'admin',
   port: 3306,
   password: '1crappypassword',
   database: 'task_app1'
